37a1ad5781af2dde6b71da5a1e9dbdb2d597e8c4
Sudron <51708062+Sudron@users.noreply.github.com>
2019-07-01 10:48:50 +0200

    Add files via upload

---
+++ stationnementParis2.py
@@ -0,0 +1,79 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Sebastien SUDRON
+#
+# Created:     20/06/2019
+# Copyright:   (c) Élève 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import pandas
+import csv
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+
+# import du fichier heart.txt avec pandas dans la variable df (df = DataFrame)
+##df = pandas.read_csv( "stationnement-sur-voie-publique-stationnement-interdit.csv", sep = ';', header = 0 , low_memory=False)
+df = pandas.read_csv("E:\greta\DataPythonCSV\stationnement-sur-voie-publique-stationnement-interdit.csv", sep = ';',header = 0,low_memory=False) # Lire un fichier texte
+
+selectArrond=input('Choisir un arraondissement')
+
+
+
+tdf=df[ ( (df["ARROND"] == int(selectArrond)) ) ].groupby(['ARROND','REGPAR']).size().reset_index(name='FREQ')
+tdfMin=tdf.groupby(["ARROND","FREQ"]).min()
+tdfMax=tdf.groupby(["ARROND","FREQ"]).max()
+tdfConc=pandas.concat([tdfMin,tdfMax], axis=0)
+
+ListTypeStationnement=[]
+##[0, 0, 0, 0, 0,0,0.4,0]listExplod
+listExplod=[]
+
+
+
+ListFrequence=[]
+for frequence in tdf['FREQ']:
+
+    ListFrequence.append(frequence)
+
+    if frequence==max(tdf['FREQ']) or frequence==min(tdf['FREQ']):
+        nbrExplod=0.5
+    else:
+        nbrExplod=0
+
+    listExplod.append(nbrExplod)
+
+for typeStationnement in tdf['REGPAR']:
+
+    ListTypeStationnement.append(typeStationnement)
+
+
+
+
+
+
+
+titre="""Les types de stationnements interdits les plus et moins populaires \n pour le """+str(selectArrond)+""" ème arrondissement de Paris """
+
+plt.figure(figsize = (5, 5))
+x = ListFrequence
+plt.pie(x, labels = ListTypeStationnement,
+           colors = ['red', 'green', 'yellow','cyan','magenta','blue','gray','brown'],
+           explode = listExplod,
+           autopct = lambda x: str(round(x, 2)) + '%',
+           pctdistance = 0.7, labeldistance = 0.8,
+           shadow = True)
+plt.legend( loc=(1,1), prop = {'size': 9})
+plt.suptitle(titre)
+
+
+
+print(selectArrond)
+
+
+plt.show()
